45a54d0cfd967048d8bb7bfeb6972ba586f55b61
Cynthia Caron <caron.cynthia@outlook.fr>
2022-01-27 16:31:56 +0100

    add auth and sauce

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
+
+const { timing } = require('npmlog');
 
 mongoose.connect('mongodb+srv://Cynthia:CynthiaDev@cluster0.tscch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -10,6 +15,7 @@ mongoose.connect('mongodb+srv://Cynthia:CynthiaDev@cluster0.tscch.mongodb.net/my
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,6 +24,10 @@ app.use((req, res, next) => {
 	next();
 })
 
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt')
-
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -17,19 +17,24 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  console.log(req.body)
+  User.findOne({ email: req.body.email }) // findOne permet de trouver un seul utilisateur dans la base de données
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: 'Utilisateur non trouvé !' });
       }
-      bcrypt.compare(req.body.password, user.password)
+      bcrypt.compare(req.body.password, user.password) // on compare le MDP envoyé avec la requête avec le MDP dans notre base de données
         .then(valid => {
           if (!valid) {
             return res.status(401).json({ error: 'Mot de passe incorrect !' });
           }
           res.status(200).json({
             userId: user._id,
-            token: 'TOKEN'
+            token: jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' }
+            )
           });
         })
         .catch(error => res.status(500).json({ error }));
