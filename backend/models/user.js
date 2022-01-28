3c432ceea1c698a5a0d63a92c81a1ddabea40f53
Cynthia Caron <caron.cynthia@outlook.fr>
2022-01-28 15:48:56 +0100

    add likes and dislikes function

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
+const userSchema = mongoose.Schema({ //création d'un schema mongoose pour nos utilisateurs
+    email: {type: String, required: true, unique: true}, // l'email doit être unique
     password: {type: String, required: true}
 });
 
